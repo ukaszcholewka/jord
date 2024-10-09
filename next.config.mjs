9a44cf3cec690e952f7a4d7d75987ada9da46ef6
Lukasz Cholewka <lukaszcholewka@loftyworks.com>
2024-10-09 12:51:35 +0200

    feat: added host from env to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+
+const HOST = process.env.NEXT_PUBLIC_HOST
+const PORT = process.env.NEXT_PUBLIC_PORT
+
 const nextConfig = {
   images: {
-    remotePatterns: [{
-      protocol: 'http',
-      hostname: 'localhost',
-      port: '3000',
-      pathname: '/photos/api/image/**'
-    }]
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/photos/**'
+      },
+      {
+        protocol: 'http',
+        hostname: HOST,
+        port: PORT,
+        pathname: '/photos/**'
+      },
+    ]
   }
 };
 

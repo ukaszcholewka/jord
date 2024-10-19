885d7e8e583c2bd9ddd341ec49469b9abfa73bb8
Lukasz Cholewka <lukaszcholewka@loftyworks.com>
2024-10-19 15:31:39 +0200

    fix: refactor of removing files

--- app/photos/api/remove/[date]/[name]/route.ts
+++ app/photos/api/remove/[date]/[name]/route.ts
@@ -1,4 +1,5 @@
 import { PHOTOS, STORAGE } from '@/constants'
+import { revalidatePath } from 'next/cache'
 import { unlink } from 'node:fs/promises'
 
 const PHOTO_DIR = `./${STORAGE}/${PHOTOS}`
@@ -7,14 +8,15 @@ export async function GET(
   _: Request,
   { params }: { params: { date: string, name: string } }
 ) {
-
   const dir = new Bun.Glob(`${PHOTO_DIR}/${params.date}/**/${params.name}.*`)
-
   for await (const file of dir.scan({
     dot: true
   })) {
     await unlink(file)
   }
 
-  return Response.json({ status: 'ok' })
+  revalidatePath('/photos', 'page')
+  return new Response(null, {
+    status: 200
+  })
 }

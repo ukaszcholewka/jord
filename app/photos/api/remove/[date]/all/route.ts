d6eea352ce3406347f268e61fd111b71e47e6506
Lukasz Cholewka <lukaszcholewka@loftyworks.com>
2024-10-21 18:08:12 +0200

    fix: refactor

--- app/photos/api/remove/[date]/all/route.ts
+++ app/photos/api/remove/[date]/all/route.ts
@@ -1,9 +1,5 @@
 import { APP_URL } from "@/api/JordApi"
-import { PHOTOS, STORAGE } from "@/constants"
-import { unlink } from "fs/promises"
 import { revalidatePath } from "next/cache"
-
-const PHOTO_DIR = `./${STORAGE}/${PHOTOS}`
 
 export async function POST(
   request: Request,
@@ -11,33 +7,9 @@ export async function POST(
 ) {
   const images = await request.json() as string[] || []
 
-
-
   for await (const image of images) {
     await fetch(`${APP_URL}/photos/api/remove/${params.date}/${image}`)
   }
-
-  // const imageList = `{${images.join(',')}}`
-
-  // console.log(`${PHOTO_DIR}/${params.date}/**/${imageList}.*`)
-
-  // const dir = new Bun.Glob(
-  //   `${PHOTO_DIR}/${params.date}/**/${imageList}.*`
-  // )
-
-  // console.log('files:')
-
-  // for await (const file of dir.scan({
-  //   dot: true
-  // })) {
-  //   console.log(file)
-  // }
-
-  // for await (const file of dir.scan({
-  //   dot: true
-  // })) {
-  //   await unlink(file)
-  // }
 
   revalidatePath('/photos', 'page')
   return new Response(null, {

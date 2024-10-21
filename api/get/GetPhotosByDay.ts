492b28404dc734b9f0cf8c570fe7666d4bd55c6d
Lukasz Cholewka <lukaszcholewka@loftyworks.com>
2024-10-21 16:01:55 +0200

    fix: added sorting

--- api/get/GetPhotosByDay.ts
+++ api/get/GetPhotosByDay.ts
@@ -1,5 +1,5 @@
 import { existsSync } from 'node:fs'
-import { readdir } from 'node:fs/promises'
+import { readdir, stat } from 'node:fs/promises'
 
 
 export type PhotosByDayList = {
@@ -11,20 +11,27 @@ export type PhotosByDayList = {
 const PHOTOS = './storage/photos'
 
 async function getPhotosBayDay(date: string) {
-
   const dir = `${PHOTOS}/${date}`
 
   if (!existsSync(dir))
     throw new Error('Missing dir')
 
   const files = await readdir(dir)
+  const stats: Record<string, any> = {}
+
+  for (let file of files) {
+    const { mtimeMs } = await stat(`${dir}/${file}`)
+    stats[file] = mtimeMs
+  }
+
   const parsed = files.map((file) => ({
+    mtime: stats[file] || 1,
     ext: /(?=[^.]*$).*/.exec(file)?.[0],
     name: /^.*(?=\.)/.exec(file)?.[0],
     date: date
   })).filter(({ ext, name, date }) => !!name || !!ext || !!date).filter(({ name }) =>
     name !== ''
-  ) as PhotosByDayList[]
+  ).sort((a, b) => a.mtime - b.mtime) as PhotosByDayList[]
 
   const sorted = Object.groupBy(parsed, ({ name }) => name)
   const singles = Array.from(new Set(parsed.map(({ name }) => name)))
